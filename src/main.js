bbbc5687461fac39623ea1cfec1c13dccb696170
Federico Leone <32718106+federicoleone@users.noreply.github.com>
2021-04-13 11:54:03 -0300

    Project kick of 🚀

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
 
+import { VueSpinners } from "@saeris/vue-spinners";
+
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+
+Vue.use(VueSpinners);
+
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percentFilter", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
